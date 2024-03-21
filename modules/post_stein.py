08506eaba53758fadcf61ca8c85c71d51672097c
yoneyama <r.yoneyama03@gmail.com>
2024-03-21 13:29:31 +0900

    ２つのシートに書き込む処理を追加

--- modules/post_stein.py
+++ modules/post_stein.py
@@ -4,8 +4,18 @@ import requests
 from dotenv import load_dotenv; load_dotenv()
 
 stein_url = os.getenv('STEIN_URL')
-
+stein_url_enbedding = os.getenv('STEIN_URL_EMB')
 def post_stein_api(data: dict):
+  return post_stein(data, stein_url)
+
+def post_stein_enb(data: list):
+  dict_data = {}
+  for i in range(len(data)):
+    dict_data[i+1] = data[i] # 1-index
+    
+  return post_stein(dict_data, stein_url_enbedding)
+
+def post_stein(data: dict, stein_url: str):
   data_str = json.dumps(data)
   response = requests.post(
     stein_url,
@@ -36,6 +46,19 @@ if __name__ == '__main__':
 
   }
 
+  test = [
+    0,
+    220,
+    0.34,
+    0.55,
+    0.55,
+    0.55,
+    0.55,
+    0.55,
+    0.55,
+    0.55,
+  ]
   res = post_stein_api(test_json)
+  # res = post_stein_enb(test)
   print(res.text)
   print(res.status_code)
